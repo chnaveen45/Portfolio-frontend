bf51da144f169b37fdd06363532a222e974a81aa
Chinthala naveen <Chinthalanaveen504@gmail.com>
2025-03-16 11:19:38 +0530

    added the fetch one

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -207,7 +207,7 @@ const Contact = () => {
       
  
       
-      const response = await fetch(`${API_URL}/Form`, {
+      const response = await fetch("https://portfolio-backend-96vr.onrender.com/Form", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
